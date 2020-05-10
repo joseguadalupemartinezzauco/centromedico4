de227e10adf2808831847c6b2ecebbb7ffd6c834
joseguadalupemartinezzauco <jose.martinez255@tesh.edu.mx>
2020-05-09 22:36:04 -0500

    Agregar archivos de proyecto.

---
+++ centromedico2/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace centromedico2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult AgregarCita()
+        {
+            return View();
+        }
+        public ActionResult citas()
+        {
+            return View();
+        }
+        public ActionResult consultorios()
+        {
+            return View();
+        }
+        public ActionResult especialidades()
+        {
+            return View();
+        }
+        public ActionResult medicos()
+        {
+            return View();
+        }
+        public ActionResult pacientes()
+        {
+            return View();
+        }
+        public ActionResult reportes()
+        {
+            return View();
+        }
+        public ActionResult usuarios()
+        {
+            return View();
+        }
+    }
+}
---
+++ centromedico2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="centromedico2.MvcApplication" Language="C#" %>
